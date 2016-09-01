332a701ef3b2907d502009318808ec1a2b6975ca
tomc944 <you@example.com>
2016-09-01 15:05:34 -0700

    Add reseed rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'set'
-
 20.times do
 	Post.create!(post: 		 							Faker::Lorem.paragraph,
 							title: 		 							Faker::Book.title,
