44c688064af5f23dc928dd6fcbf2981acb369d61
tomc944 <thomas.claiborne3@gmail.com>
2016-10-01 18:20:02 -0700

    Adds ellipses styling

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -7,9 +7,9 @@ const PostIndexItem = React.createClass({
     const postLink = "/posts/" + this.props.post.id;
 
     return (
-      <div className="report-item">
+      <div className="ellipsis">
         <h1 className='report-title'><Link to={postLink}>{this.props.post.title}</Link></h1>
-        <p>{this.props.post.post}</p>
+        <p className='report-body'>{this.props.post.post}</p>
       </div>
     )
   }
